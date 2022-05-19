84968eebc2ed995c2ed5bc59a9a352d9d1882341
amyd11 <ald5044@gmail.com>
2022-05-19 10:59:29 -0400

    Feature/843 set up routing (#12)
    * add react router to the application to manage routing
    
    * adding routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import { AppRoutes } from "./AppRoutes";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <button className="usa-button usa-button--big">Big USWDS Button</button>
       </header>
+      <AppRoutes />
     </div>
   );
 }
