a366fcbd58c3b6167bdb531473c69fa106a277ba
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-05-19 18:54:35 -0400

    915 creating static pages

---
+++ src/Help.jsx
@@ -0,0 +1,45 @@
+const Help = () => {
+    return (
+        <>
+            <h1>Help</h1>
+            <p>Browse the FAQs for for quick answers or visit the website for in-depth information. Contact us directly if you have a comment or don’t find what you’re looking. Thank you for using Blueprint!</p>
+            <div className="usa-card__container">
+                <div className="usa-card__header">
+                    <h3>Frequently Asked Questions</h3>
+                </div>
+                <div className="usa-card__body">
+                    <span>Most popular inquiries</span>
+                </div>
+                <div class="usa-card__footer">
+                    <a href="/faq"><button className="usa-button usa-button--big">Read the FAQs</button></a>
+                </div>
+            </div>
+
+            <div className="usa-card__container">
+                <div className="usa-card__header">
+                    <h3>About CMS Rapid ATO</h3>
+                </div>
+                <div className="usa-card__body">
+                    <span>Read more about the inititive</span>
+                </div>
+                <div class="usa-card__footer">
+                <a href="https://ato.cms.gov/"><button className="usa-button usa-button--big">Learn more</button></a>
+                </div>
+            </div>
+            
+            <div className="usa-card__container">
+                <div className="usa-card__header">
+                    <h3>Contact Us</h3>
+                </div>
+                <div className="usa-card__body">
+                    <span>We look forward to connecting</span>
+                </div>
+                <div class="usa-card__footer">
+                <a href="/contactus"><button className="usa-button usa-button--big">Contact us</button></a>
+                </div>
+            </div>
+        </>
+    );
+  };
+  
+  export default Help;
