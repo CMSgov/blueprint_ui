f230a971214147d177f7a27a9009c357cadbf70b
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-05-26 10:26:41 -0400

    header styleing fixes, unit test fix, and jsx fix

--- src/organisms/Header.jsx
+++ src/organisms/Header.jsx
@@ -1,4 +1,4 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../atoms/Logo";
 import UserStub from "../stub";
 
@@ -10,15 +10,16 @@ const Header = () => {
         <header className="usa-header site-header site-header-dark" role="banner">
             <div className="usa-navbar site-header-navbar">
                 <div id="logo">
-                    <a href="/">
+                    <Link to="/">
                         <Logo />
-                    </a>
+                    </Link>
                 </div>
-                <a href="/projects">Projects</a>
+                
+                <Link to="/projects">Projects</Link>
                 {/* @TODO: change component library to a link when it exists */}
                 <a href="/componentLibrary">Component Library</a>
                 <div className="site-header-right">
-                    <a href="/help">Help</a>
+                    <Link to="/help">Help</Link>
                     <span className="usa-media-block__img circle-4 bg-base display-flex flex-row flex-align-center flex-justify-center text-white">{count}</span>
                     {/* @TODO: change user to a link when it exists */}
                     <a href="/user">{userName}</a>
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,18 +4,18 @@ const ContactUs = () => {
         <h1>Contact Us</h1>
         <p>Complete this form or email us directly at <a href="mailto:rapidato@cms.hhs.gov">rapidato@cms.hhs.gov</a></p>
         {/* @TODO: need to change the form action to submit to yet to be determined endpoint for storage in the DB and email */}
-        <form action="mailto:rapidato@cms.hhs.gov" method="post" enctype="text/plain" className="usa-form usa-form--large">
-            <label className="usa-label" for="fname">First name <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+        <form action="mailto:rapidato@cms.hhs.gov" method="post" encType="text/plain" className="usa-form usa-form--large">
+            <label className="usa-label" htmlFor="fname">First name <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <input className="usa-input" type="text" id="fname" name="fname" required />
-            <label className="usa-label" for="lname">Last name <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+            <label className="usa-label" htmlFor="lname">Last name <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <input className="usa-input" type="text" id="lname" name="lname" required />
-            <label className="usa-label" for="org">Organization/Department <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+            <label className="usa-label" htmlFor="org">Organization/Department <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <input className="usa-input" type="text" id="org" name="org" required />
-            <label className="usa-label" for="email">Email <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+            <label className="usa-label" htmlFor="email">Email <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <input className="usa-input" type="email" id="email" name="email" required />
-            <label className="usa-label" for="phone">Phone number <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+            <label className="usa-label" htmlFor="phone">Phone number <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <input className="usa-input" type="phone" id="phone" name="phone" required />
-            <label className="usa-label" for="comment">Please enter your questions, comments, or feedback and we will be in touch soon <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
+            <label className="usa-label" htmlFor="comment">Please enter your questions, comments, or feedback and we will be in touch soon <abbr title="required" className="usa-hint usa-hint--required">*</abbr></label>
             <textarea id="comment" name="comment" required ></textarea><br />
             <input type="submit" value="Submit" className="usa-button usa-button--big" />
         </form>
