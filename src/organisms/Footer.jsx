1f2680cee999621121256dcb2695632a3cfe1215
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-05-27 14:55:12 -0400

    removed version and copywrite from footer

--- src/organisms/Footer.jsx
+++ src/organisms/Footer.jsx
@@ -7,10 +7,6 @@ const Footer = () => {
                 <Link to={{ pathname: "https://ato.cms.gov/" }} target="_blank">About Rapid ATO</Link>
                 <Link to="/help">Blueprint Support</Link>
             </div>
-            <div className="footer-stats">
-                <span className="footer-item">© GovReady PBC 2022</span>
-                <span className="footer-item">v0.9.12-dev</span>
-            </div>
         </footer>
     );
 };
