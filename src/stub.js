72127d52a65aa908e6b032f988d93cd674e1e369
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-05-27 09:20:44 -0400

    Header component and styling page elements with grid layout, also som… (#14)
    * Header component and styling page elements with grid layout, also some other uswds style fixes
    
    * added unit test to header, changed Link to a tags
    
    * header styleing fixes, unit test fix, and jsx fix

---
+++ src/stub.js
@@ -0,0 +1,10 @@
+ const UserStub = {
+    "id": 1,
+    "username": "Dick",
+    "first_name": "",
+    "last_name": "",
+    "email": "r.moore@theta-llc.com",
+    "is_active": true
+};
+
+export default UserStub;
