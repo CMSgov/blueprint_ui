3c80680ef2c36e7e74b62079e22396ea28fb5558
TMCamp <77361948+TMCamp@users.noreply.github.com>
2022-06-02 16:08:18 -0400

    Revert "Utilizing trussworks uswds components, setting up centralized breadcr…" (#22)
    Co-authored-by: Joshua Alley <jauschalley@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import "./App.scss";
 import Footer from "./organisms/Footer";
 import { Outlet } from "react-router-dom";
 import Header from "./organisms/Header";
-import { useState } from "react";
-import Breadcrumbs from "./organisms/Breadcrumbs";
-import GlobalState from "./GlobalState";
+import "./App.scss";
 
 function App() {
-  const [state, setState] = useState({});
   return (
     <div className="App">
-      <GlobalState.Provider value={[state, setState]}>
-        <Header />
-        <div className="grid-row grid-col-12 main-content-body">
-          <Breadcrumbs />
-          <Outlet />
-        </div>
-        <Footer />
-      </GlobalState.Provider>
+      <Header />
+      <div className="grid-row grid-col-12 main-content-body">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
