20e031cb57892564cd03a00545adc119e5e0b2c5
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-06-14 14:04:10 -0400

    Feature/948 breadcrumbs again (#26)
    * recommit for breadcrumbs
    
    * footer ui fix
    
    * footer adjustment
    
    * build issue resolution
    
    * Updating FAQ and Contact Us urls (#27)
    
    * recommit for breadcrumbs
    
    * footer ui fix
    
    * footer adjustment
    
    * build issue resolution
    
    * updated breadcrumbs to behave correctly with updated help sub pages
    
    * reusing app router list, made some functions to handle setting values

--- cypress/integration/placeholder/test.spec.js
+++ cypress/integration/placeholder/test.spec.js
@@ -1,10 +1,68 @@
 /// <reference types="cypress" />
 
-// remove after adding first real test
-
-describe("sample test on react default", () => {
-  it("renders basic elements of react default page", () => {
+describe("breadcrumbs & h1 show correctly on home page", () => {
+  it("does not display on home page", () => {
     cy.visit(Cypress.env("BASE_URL"));
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("not.exist");
+    cy.get("h1").contains("Welcome to Blueprint for CMS");
     cy.get(".usa-process-list").should("exist");
+    cy.get("footer").should("exist");
+  });
+});
+
+describe("breadcrumbs & h1 show correctly on help page", () => {
+  it("does display on help page", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/help");
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("exist").contains("Home");
+    cy.get(".usa-breadcrumb").contains("help");
+    cy.get("h1").contains("Help");
+    cy.get("footer").should("exist");
+  });
+});
+
+describe("breadcrumbs & h1 show correctly on faq page", () => {
+  it("does display on faq page", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/help/faq");
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("exist").contains("Home");
+    cy.get(".usa-breadcrumb").contains("help");
+    cy.get(".usa-breadcrumb").contains("faq");
+    cy.get("h1").contains("Frequently Asked Questions");
+    cy.get("footer").should("exist");
+  });
+});
+
+describe("breadcrumbs & h1 show correctly on contact us page", () => {
+  it("does display on contact us page", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/help/contact-us");
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("exist").contains("Home");
+    cy.get(".usa-breadcrumb").contains("help");
+    cy.get(".usa-breadcrumb").contains("contact us");
+    cy.get("h1").contains("Contact Us");
+    cy.get("footer").should("exist");
+  });
+});
+
+describe("breadcrumbs show correctly on projects page", () => {
+  it("does display on projects list page", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/projects");
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("exist").contains("Home");
+    cy.get(".usa-breadcrumb").contains("projects");
+    cy.get("footer").should("exist");
+  });
+});
+
+describe("breadcrumbs show correctly on projects not found page", () => {
+  it("does display on project not found page", () => {
+    cy.visit(Cypress.env("BASE_URL") + "/projects/0");
+    cy.get("header").should("exist");
+    cy.get(".usa-breadcrumb").should("exist").contains("Home");
+    cy.get(".usa-breadcrumb").contains("projects");
+    cy.get("h1").contains("Project not found");
+    cy.get("footer").should("exist");
   });
 });
---
+++ src/organisms/Breadcrumbs.jsx
@@ -0,0 +1,93 @@
+import {
+  BreadcrumbBar,
+  Breadcrumb,
+  BreadcrumbLink,
+} from "@trussworks/react-uswds";
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import GlobalState from "../GlobalState";
+import { MAIN_ROUTES } from "../AppRoutes";
+
+const ParentBreadcrumbs = ({ parent }) => {
+  return <BreadcrumbLink href={parent.url}>{parent.title}</BreadcrumbLink>;
+};
+
+const Breadcrumbs = () => {
+  const params = useLocation();
+  const homePageName = "Home";
+  const pathname = params.pathname.replaceAll("/", " ").trim();
+  let topLevelList = [];
+  for (let key in MAIN_ROUTES) {
+    topLevelList.push(MAIN_ROUTES[key].replaceAll("/", " ").trim());
+  }
+  const [state] = useContext(GlobalState);
+
+  if (pathname === "") {
+    // Do not show breadcrumbs on the home page
+    return "";
+  } else if (topLevelList.includes(pathname)) {
+    return (
+      <BreadcrumbBar>
+        <Breadcrumb>
+          <BreadcrumbLink href="/">{homePageName}</BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb current>{pathname.replaceAll("-", " ")}</Breadcrumb>
+      </BreadcrumbBar>
+    );
+  }
+  let parentList = [];
+  let pathArray = pathname.split(" ");
+  let pageName = "";
+
+  function appendToParentList(title, url) {
+    parentList.push({
+      title: title,
+      url: url,
+    });
+  }
+  function setPageName(name) {
+    pageName = name.replaceAll("-", " ");
+  }
+  setPageName(pathArray[pathArray.length - 1]);
+  if (pathArray[1] === "contact-us" || pathArray[1] === "faq") {
+    appendToParentList("help", "/help");
+  } else if (pathArray[0] === "component-library") {
+    appendToParentList("component library", "/component-library");
+    // @TODO: when we have a component module convert pathArray[1] to component.title
+    // should be similar to the project section pulling from the global state
+  } else if (pathArray[0] === "project-setup") {
+    appendToParentList("project setup", "/project-setup");
+    if (pathArray.length === 3) {
+      appendToParentList("Confirmation", "/project-setup/confirmation");
+    }
+  } else if (pathArray[0] === "projects") {
+    appendToParentList("projects", "/projects");
+    let title = "";
+    if (state.project !== undefined && state.project.title !== undefined) {
+      title = state.project.title;
+    }
+
+    if (pathArray.length === 2) {
+      setPageName(title);
+    } else if (pathArray.length === 3) {
+      //potential pages they are on "Add Team Member", "Project Team", "Project Settings", "System Security Plan", "Manage Components", "System Controls"
+      appendToParentList(title, `/projects/${pathArray[1]}`);
+    }
+  }
+
+  return (
+    <BreadcrumbBar>
+      <Breadcrumb>
+        <BreadcrumbLink href="/">{homePageName}</BreadcrumbLink>
+      </Breadcrumb>
+      {parentList.map((parent, i) => (
+        <Breadcrumb key={i}>
+          <ParentBreadcrumbs parent={parent} />
+        </Breadcrumb>
+      ))}
+      <Breadcrumb current>{pageName}</Breadcrumb>
+    </BreadcrumbBar>
+  );
+};
+
+export default Breadcrumbs;
