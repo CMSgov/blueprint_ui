2301ed08745ee217e584a375bf315c51e25c9aeb
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-06-14 11:18:18 -0400

    reusing app router list, made some functions to handle setting values

--- src/organisms/Breadcrumbs.jsx
+++ src/organisms/Breadcrumbs.jsx
@@ -6,6 +6,7 @@ import {
 import { useContext } from "react";
 import { useLocation } from "react-router-dom";
 import GlobalState from "../GlobalState";
+import { MAIN_ROUTES } from "../AppRoutes";
 
 const ParentBreadcrumbs = ({ parent }) => {
   return <BreadcrumbLink href={parent.url}>{parent.title}</BreadcrumbLink>;
@@ -15,12 +16,10 @@ const Breadcrumbs = () => {
   const params = useLocation();
   const homePageName = "Home";
   const pathname = params.pathname.replaceAll("/", " ").trim();
-  const topLevelList = [
-    "help",
-    "projects",
-    "project-setup",
-    "component-library",
-  ];
+  let topLevelList = [];
+  for (let key in MAIN_ROUTES) {
+    topLevelList.push(MAIN_ROUTES[key].replaceAll("/", " ").trim());
+  }
   const [state] = useContext(GlobalState);
 
   if (pathname === "") {
@@ -40,36 +39,39 @@ const Breadcrumbs = () => {
   let pathArray = pathname.split(" ");
   let pageName = "";
 
+  function appendToParentList(title, url) {
+    parentList.push({
+      title: title,
+      url: url,
+    });
+  }
+  function setPageName(name) {
+    pageName = name.replaceAll("-", " ");
+  }
+  setPageName(pathArray[pathArray.length - 1]);
   if (pathArray[1] === "contact-us" || pathArray[1] === "faq") {
-    parentList = [{ title: "help", url: "/help" }];
-    pageName = pathArray[1].replaceAll("-", " ");
+    appendToParentList("help", "/help");
   } else if (pathArray[0] === "component-library") {
-    parentList = [{ title: "component library", url: "/component-library" }];
-    //@TODO: when we have a component module convert pathArray[1] to component.title
+    appendToParentList("component library", "/component-library");
+    // @TODO: when we have a component module convert pathArray[1] to component.title
     // should be similar to the project section pulling from the global state
-    pageName = pathArray[1];
   } else if (pathArray[0] === "project-setup") {
-    parentList = [{ title: "project setup", url: "/project-setup" }];
+    appendToParentList("project setup", "/project-setup");
     if (pathArray.length === 3) {
-      parentList.push({
-        title: "Confirmation",
-        url: "/project-setup/confirmation",
-      });
+      appendToParentList("Confirmation", "/project-setup/confirmation");
     }
-    pageName = pathArray[pathArray.length - 1];
   } else if (pathArray[0] === "projects") {
-    parentList = [{ title: "projects", url: "/projects" }];
+    appendToParentList("projects", "/projects");
     let title = "";
     if (state.project !== undefined && state.project.title !== undefined) {
       title = state.project.title;
     }
 
     if (pathArray.length === 2) {
-      pageName = title;
+      setPageName(title);
     } else if (pathArray.length === 3) {
       //potential pages they are on "Add Team Member", "Project Team", "Project Settings", "System Security Plan", "Manage Components", "System Controls"
-      parentList.push({ title: title, url: `/projects/${pathArray[1]}` });
-      pageName = pathArray[pathArray.length - 1];
+      appendToParentList(title, `/projects/${pathArray[1]}`);
     }
   }
 
