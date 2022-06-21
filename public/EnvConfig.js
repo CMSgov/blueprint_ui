926f1a128b10724037c38ce1007f2eb9ba282956
rapidato-github-svc <93146671+rapidato-github-svc@users.noreply.github.com>
2022-06-21 17:21:50 -0400

    Feature/UI env am (#32)
    * ISPGBSS-1028 added env-config.js file and updated index.html
    
    * ISPGBSS-1028 updated config.js
    
    * ISPGBSS-1028 Changed js config file name.
    
    * ISPGBSS-1028 Removed NODE_ENV var.
    
    * ISPGBSS-1028 Moved EnvConfig.js to top of head section.
    
    Co-authored-by: Azhar Mian <azharem@gmail.com>

---
+++ public/EnvConfig.js
@@ -0,0 +1,4 @@
+window._env_ = {
+	REACT_APP_API_ENDPOINT:'http://localhost:8000',
+	REACT_APP_ENVIRONMENT:'dev'
+}
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line
 export default {
-  backendUrl: `${process.env.REACT_APP_API_ENDPOINT}/api`,
+  backendUrl: `${window._env_.REACT_APP_API_ENDPOINT}/api`,
 };
