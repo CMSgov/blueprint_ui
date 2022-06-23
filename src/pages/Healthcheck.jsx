cce9f39c234b1a54100ddace474529011a4dc27a
rapidato-github-svc <93146671+rapidato-github-svc@users.noreply.github.com>
2022-06-23 10:54:07 -0400

    Add healthcheck route and call API healthcheck (#33)
    * ISPGBSS-1032 Added healthcheck.
    
    * ISPGBSS-1032 Added error handling.
    
    Co-authored-by: Azhar Mian <azharem@gmail.com>

---
+++ src/pages/Healthcheck.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect} from "react";
+import config from "../config";
+
+const Healthcheck = () => {
+    const [error, setError] = useState(false);
+    const [status, setState] = useState('');
+    useEffect(() => {
+        if (status === '' ) {
+            fetch(`${config.backendUrl}/healthcheck/`, {
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            })
+                .then((healthcheck) => {
+                    return setState(healthcheck.status);
+                })
+                .catch((error) => {
+                    return setError(true);
+                });
+        }
+    }, [status, setState]);
+    if (error) {
+        return <h1>Api healthcheck is not reachable</h1>;
+    }
+    return (
+        <>
+            <h1>Api healthcheck status: {status}</h1>
+        </>
+    );
+};
+export default Healthcheck;
