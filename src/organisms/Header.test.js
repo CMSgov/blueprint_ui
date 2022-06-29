c70708a9c0245b7686a8041e21e428834050bde5
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-06-29 11:08:24 -0400

    removing commented out code

--- src/organisms/Header.test.js
+++ src/organisms/Header.test.js
@@ -2,27 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Header from "./Header";
 
-// let windowSpy;
-
-// beforeEach(() => {
-//   windowSpy = jest.spyOn(window, "window", "_env_");
-// });
-
-// afterEach(() => {
-//   windowSpy.mockRestore();
-// });
-
 test("renders links", () => {
-  // windowSpy.mockImplementation(() => ({
-  //     _env_: {
-  //         REACT_APP_API_ENDPOINT:'http://localhost:8000'
-  //     }
-  //   }));
-  // Object.defineProperty(window, '_env_', {
-  //     value: {
-  //         REACT_APP_API_ENDPOINT:'http://localhost:8000'
-  //     }
-  //   });
   render(<Header />, { wrapper: MemoryRouter });
   const helpLinkElement = screen.getByText(/help/i);
   expect(helpLinkElement).toBeInTheDocument();
