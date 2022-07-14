9869645568ca9d5d95dbdb1fd41388e29f62dcb8
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-07-14 12:46:48 -0400

    pulling in all components to choose from on component selection step, plus some adjustments based on HCD

--- src/pages/ProjectSetupSelectComponents.jsx
+++ src/pages/ProjectSetupSelectComponents.jsx
@@ -1,10 +1,22 @@
+import { Checkbox } from "@trussworks/react-uswds";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import Config from "../config";
+
 const TableRow = ({ component }) => {
   if (component.title === undefined) {
     return "";
   }
   return (
     <>
-      <td></td>
+      {/* label might be required for checkbox */}
+      <td>
+        <Checkbox
+          id={"component-id-" + component.id}
+          name="component-id-list"
+          value={component.id}
+        />
+      </td>
       <td>{component.title}</td>
       <td>{component.description}</td>
       <td>{component.controls_count}</td>
@@ -13,10 +25,76 @@ const TableRow = ({ component }) => {
 };
 
 const ProjectSetupSelectComponents = () => {
-  // @TODO: Settup the data for this placeholder page use the local storage to get the components
-  const componentList = [];
+  const [componentList, setComponentList] = useState([]);
+  const [error, setError] = useState(false);
+  const [response, setResponse] = useState(false);
+  const localStorageProject = JSON.parse(localStorage.getItem("project"));
+  const projectId = localStorageProject.id;
+  const projectUrl = "/projects/" + projectId;
+  useEffect(() => {
+    fetch(`${Config("backendUrl")}/components/search/`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((componentList) => {
+        if (componentList !== undefined) {
+          return setComponentList(componentList);
+        } else {
+          return setError(true);
+        }
+      })
+      .catch((error) => {
+        return setError(true);
+      });
+  }, []);
+
+  function postProjectAddComponent(formValues) {
+    fetch(`${Config("backendUrl")}/projects/add-component/`, {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(formValues),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        return setResponse(true);
+      })
+      .catch((error) => {
+        return setError(error);
+      });
+  }
+
+  const formSubmit = () => {
+    let postVariables = {};
+    postVariables["project_id"] = projectId;
+    postVariables["creator"] = 1; // @TODO: change for your local environment/demo
+    let checkedList = document.querySelectorAll(
+      "input[type='checkbox']:checked"
+    );
+    checkedList.forEach(getValue);
+    function getValue(checkbox) {
+      postVariables["component_id"] = checkbox.value;
+      postProjectAddComponent(postVariables);
+    }
+    console.log(checkedList.length);
+    if (checkedList.length === 0) {
+      setResponse(true);
+    }
+  };
+
+  let saved = false;
+  if (response) {
+    saved = true;
+  }
+
   return (
-    <>
+    <div className="select-components">
+      {saved && <Navigate to={projectUrl} />}
       <h1>Select components for your system technologies</h1>
       <ul class="usa-list">
         <li>
@@ -52,10 +130,10 @@ const ProjectSetupSelectComponents = () => {
       <a href="/project-setup/confirmation">
         <button className="usa-button usa-button--outline">Back</button>
       </a>
-      <a href="/project/{project.id}">
-        <button className="usa-button">Confirm</button>
-      </a>
-    </>
+      <button className="usa-button" onClick={formSubmit}>
+        Confirm
+      </button>
+    </div>
   );
 };
 
