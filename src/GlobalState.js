a8a4a2950cdbeee23de7d7534ac713483aa88094
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-08-05 11:34:22 -0400

    refactored global state to be able to run projectsettings.test.jsx

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
-const GlobalState = React.createContext([{}, () => {}]);
+const GlobalState = React.createContext();
+
+export const GlobalStateProvider = ({ children }) => {
+  const [state, setState] = useState({});
+  return (
+    <GlobalState.Provider value={[state, setState]}>
+      {children}
+    </GlobalState.Provider>
+  );
+};
 
 export default GlobalState;
